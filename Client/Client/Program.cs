ff87445b8d537ad5e4e9cf5d6ad3980340cf697c
TIcolorobbia <72518893+TIcolorobbia@users.noreply.github.com>
2024-05-24 17:18:38 -0300

    Commit inicial

---
+++ Client/Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+//Configuração do Servidor
+const string ENDERECO_IP = "127.0.0.1";
+const int PORTA_CONEXAO = 8181;
+
+
+Console.WriteLine("Iniciando Cliente...");
+Console.WriteLine($"Conectando no servidro {ENDERECO_IP} porta {PORTA_CONEXAO}");
+
+IPHostEntry ipHostInfo = Dns.GetHostByAddress(ENDERECO_IP);
+IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+var ipEndPoint = new IPEndPoint(ipAddress, PORTA_CONEXAO);
+
+
+while (true)
+{
+    try
+    {
+        //Inicia o TCPClient
+        using TcpClient client = new();
+
+        //Conecta no servidor
+        await client.ConnectAsync(ipEndPoint);
+
+        //Abre o Stream de conecxão 
+        await using NetworkStream stream = client.GetStream();
+
+        //Faz a leitura da mensagem enviada do servidor 
+        var buffer = new byte[1024];
+        int received = await stream.ReadAsync(buffer);
+
+        var message = Encoding.UTF8.GetString(buffer, 0, received);
+        if (!message.Equals("<|CONTINUE|>"))
+            Console.WriteLine(message);
+
+        //Pede um mensagem para ser enviada ao servidor 
+        Console.WriteLine("Escreva a sua mensagem:");
+        message = Console.ReadLine();
+        await stream.WriteAsync(Encoding.UTF8.GetBytes(message));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Conexão encerrada pelo servidor. {ex.Message}");
+        break;
+    }
+
+}
---
+++ Server/Program.cs
@@ -0,0 +1,65 @@
+﻿//Cria um servidor
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+//Porta de conexão do Servidor 
+const int PORTA_CONEXAO = 8181;
+
+Console.WriteLine($"Iniciando Servidor. Escutando porta: {PORTA_CONEXAO}" );
+
+//Obtem os dados do servidor e define a porta de conexão 
+var ipEndPoint = new IPEndPoint(IPAddress.Any, PORTA_CONEXAO);
+
+//Inicia listner que responsável por receber conexão 
+TcpListener listener = new(ipEndPoint);
+listener.Start();
+
+
+var message = $"Servidor Iniciado, aguardando mensagens do cliente";
+
+
+while (true)
+{
+	try
+	{
+        //Indica que vai receber qualquer conxão 
+        using TcpClient handler = await listener.AcceptTcpClientAsync();
+
+        //Abre o stream de conxão 
+        await using NetworkStream stream = handler.GetStream();
+
+        //codifia a mensagem 
+        var msgEconding = Encoding.UTF8.GetBytes(message);
+
+        //Envia a mensagem ao Cliente
+        await stream.WriteAsync(msgEconding);
+
+        //Faz a leitura da mensagem enviada pelo cliente
+        var buffer = new byte[1024];
+        int received = await stream.ReadAsync(buffer);
+
+        //escreve a mensagem recebida
+        message = Encoding.UTF8.GetString(buffer, 0, received);
+        Console.WriteLine(message);
+        message = "<|CONTINUE|>";
+
+
+        if (message.Trim().Equals("<|STOP|>"))
+        {
+            await stream.WriteAsync(Encoding.UTF8.GetBytes("Servidor finalizando..."));
+            listener.Stop();
+            break;
+        }
+    }
+	catch (Exception ex)
+	{
+
+        Console.WriteLine($"{ex.Message}");
+	}
+       
+}
+
+
